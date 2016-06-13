230f8c08f0628d87afcd581a588273bca6eb5619
Joel Vazquez <joelomar09@gmail.com>
2016-06-13 19:53:46 -0400

    Update server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var fs = require('fs');
+var file = fs.readFileSync('index.html');
 var server = http.createServer(function (req, res) {
      
      console.log('User Request');
-     res.end('Hello world');
+     res.writeHead(200, {'content-type': 'text/plain'});
+     res.end(file);
 
 
 }).listen(3000);
